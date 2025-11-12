312e4af4babeaa7ace67dc8d311b2d7e1ae187f5
Onkar Chougule <onkarchougule99@gmail.com>
2025-11-12 16:36:11 +0530

    workspace creation function added

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -53,5 +53,88 @@ const syncUserUpdate = inngest.createFunction(
   }
 );
 
+const syncWorkspaceCreate = inngest.createFunction(
+  { id: "create-workspace-with-clerk" },
+  { event: "clerk/organization.created" },
+  async ({ event }) => {
+    const { data } = event;
+    await prisma.workspace.create({
+      data: {
+        id: data?.id,
+        name: data?.name,
+        slug: data?.slug,
+        ownerId: data?.created_by,
+        image_url: data?.image_url,
+      },
+    });
+
+    // Add creator as ADMIN....
+    await prisma.workspaceMember.create({
+      data: {
+        userId: data?.created_by,
+        workspaceId: data?.Id,
+        Role: "ADMIN",
+      },
+    });
+  }
+);
+
+const syncWorkspaceUpdate = inngest.createFunction(
+  { id: "update-workspace-with-clerk" },
+  { event: "clerk/organization.updated" },
+
+  async ({ event }) => {
+    const { data } = event;
+    await prisma.workspace.update({
+      where: {
+        id: data?.id,
+      },
+      data: {
+        name: data?.name,
+        slug: data?.slug,
+        image_url: data?.image_url,
+      },
+    });
+  }
+);
+
+const syncUserworkspaceDelete = inngest.createFunction(
+  { id: "delete-workspace-with-clerk" },
+  { event: "clerk/organization.deleted" },
+
+  async ({ event }) => {
+    const { data } = event;
+    await prisma.workspace.delete({
+      where: {
+        id: data?.id,
+      },
+    });
+  }
+);
+
+//   workspace members Invitation.....
+const syncWorkspaceMemberInvitation = inngest.createFunction(
+  { id: "create-workspace-member-with-clerk" },
+  { event: "clerk/organizationInvitation.accepted" },
+  async ({ event }) => {
+    const { data } = event;
+    await prisma.workspaceMember.create({
+      data: {
+        userId: data?.userId,
+        workspaceId: data?.workspaceId,
+        role: data?.String(data?.role).toUpperCase(),
+      },
+    });
+  }
+);
+
 // Create an empty array where we'll export future Inngest functions
-export const functions = [syncUserCreate, syncUserUpdate, syncUserDelete];
+export const functions = [
+  syncUserCreate,
+  syncUserUpdate,
+  syncUserDelete,
+  syncWorkspaceCreate,
+  syncWorkspaceUpdate,
+  syncUserworkspaceDelete,
+  syncWorkspaceMemberInvitation,
+];
